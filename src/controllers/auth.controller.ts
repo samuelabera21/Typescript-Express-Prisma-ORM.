378dca2a1a842ab95caab431419e5ff82f7308f5
Samuel Abera <sami21.good.bad@gmail.com>
2026-03-08 16:08:47 +0300

    create a service to handle logic

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,18 +1,11 @@
-// import { Request, Response } from "express";
 import { RequestHandler } from "express";
+import { registerUser } from "../services/auth.service";
 
-// export const register = (req: Request, res: Response) => {
-//   res.send("Register endpoint working");
-// };
-export const register: RequestHandler = (req, res) => {
-  res.send("Register endpoint working");
+export const register: RequestHandler = async (req, res) => {
+  const result = await registerUser();
+
+  res.send(result);
 };
-
-
-
-// export const login = (req: Request, res: Response) => {
-//   res.send("Login endpoint working");
-// };
 
 export const login: RequestHandler = (req, res) => {
   res.send("Login endpoint working");
