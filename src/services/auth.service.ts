3b2cf534ed158314f47ba5b0ae978446763f0c4d
Samuel Abera <sami21.good.bad@gmail.com>
2026-03-08 16:31:13 +0300

    email already exist lojic added inside service

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,7 +1,22 @@
+import prisma from "../config/prisma";
+
+
 export const registerUser = async (
   email: string,
   password: string,
   name?: string
 ) => {
+
+    //here i need to check email is already exist or not
+        const existingUser = await prisma.users.findUnique({
+        where: { email },
+        });
+
+        if (existingUser) {
+        return "User already exists";
+        }
+
+
+  
   return "Register logic coming soon";
 };
