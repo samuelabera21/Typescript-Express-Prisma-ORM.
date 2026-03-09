f06812f0a14b14a20ac3a903670ff26d6fba01fc
Samuel Abera <sami21.good.bad@gmail.com>
2026-03-09 20:37:31 +0300

    update the cores because the back and front use different port so they should be communicate with core other wise browser block them

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,13 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
